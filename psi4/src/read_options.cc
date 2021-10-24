6ce4b1ba3da443e075a1e63a45af9db9b31b65ca
Filip Brzęk <filip.brzek@gmail.com>
2021-10-24 20:06:57 +0200

    register new global option in SAPT module

--- psi4/src/read_options.cc
+++ psi4/src/read_options.cc
@@ -933,6 +933,9 @@ int read_options(const std::string &name, Options &options, bool suppress_printi
         /*- For SAPT(DFT) computes the $S^{inf}$ Exchange-Dispersion terms. !expert -*/
         options.add_bool("DO_DISP_EXCH_SINF", false);
 
+        /*- For SF-SAPT computes only coupled induction (CPROHF) correction !expert -*/
+        options.add_bool("DO_ONLY_CPHF", false);
+
         /*- Do use asynchronous disk I/O in the solution of the CPHF equations?
         Use may speed up the computation slightly at the cost of spawning an
         additional thread. -*/
