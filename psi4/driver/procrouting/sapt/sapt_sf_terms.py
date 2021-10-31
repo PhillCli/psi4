4c411cce19d86064afbc0d88b7ed99505b193c65
Filip Brzęk <filip.brzek@gmail.com>
2021-10-31 19:46:41 +0100

    change test molecule for debugg purposes: vol1

--- psi4/driver/procrouting/sapt/sapt_sf_terms.py
+++ psi4/driver/procrouting/sapt/sapt_sf_terms.py
@@ -446,6 +446,8 @@ def compute_cphf_induction(cache, jk, maxiter: int = 100, conv: float = 1e-6):
     # omega_jj | omega_js
     # NOTE: output socc x socc (omega_ii) is always set to zero by ROHF.Hx
 
+    print(f"{rhs_A.np.shape}")
+    print(f"{rhs_B.np.shape}")
     t_A, t_B = _sapt_cpscf_solve(cache, jk, rhs_A, rhs_B, maxiter, conv)
     print(f"{t_A.np.shape=}")
     print(f"{t_B.np.shape=}")
