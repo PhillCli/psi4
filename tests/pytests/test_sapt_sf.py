e2fac9da1eef7918a656eff7439d833783f711e6
Filip Brzęk <filip.brzek@gmail.com>
2021-11-01 07:43:32 +0100

    move sf_sapt cphf tests exclusively to pytest

--- tests/pytests/test_sapt_sf.py
+++ tests/pytests/test_sapt_sf.py
@@ -3,33 +3,55 @@ from .utils import *
 
 import psi4
 
+# NOTE: results verified with finite-field custom code
+li_na_simple = """
+0 2
+Li 0.000 0.000 -2.000
+--
+0 2
+Na 0.000 0.000 +2.000
 
-# TODO: rodnik NH
-def test_sapt_sf_rohf_cph():
+no_reorient
+no_com
+units angstrom
+symmetry c1
+"""
+
+# NOTE: source http://dx.doi.org/10.1063/1.2968556
+# TODO: add NH system as well
+cn_ne_zuchowski2008 = """
+0 2
+C  0.000000 0.000000 +0.000000000
+N  0.000000 0.000000 +2.210000000
+--
+0 1
+Ne 0.000000 0.000000 -6.410228688
+
+units bohr
+symmetry c1
+no_reorient
+no_com
+"""
+
+reference_data = [
+    (
+
+    )
+]
+def test_sapt_sf_rohf_cphf():
     """test cp-rohf solver for SAPT-SF"""
 
-    psi4.geometry("""
-    0 2
-    H 0.000 0.000 -2.000
-    --
-    0 2
-    H 0.000 0.000 +2.000
-
-    no_reorient
-    no_com
-    units angstrom
-    symmetry c1
-    """)
+    psi4.geometry(li_na_simple)
 
     psi4.set_options({
-        'scf_type': 'df',
+        'scf_type': 'direct',
         'reference': 'rohf',
         'guess': 'sad',
-        'basis': 'sto-3g',
+        'basis': 'aug-cc-pvdz',
         'e_convergence': 1e-10,
         'd_convergence': 1e-10,
-        'maxiter': 100,
-        'DO_ONLY_CPHF': True,
+        'maxiter': 200,
+        'SF_SAPT_DO_ONLY_CPHF': True,
     })
 
     energy = psi4.energy('SF-SAPT')
