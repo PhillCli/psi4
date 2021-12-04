843872f485384eb671f7792ca7f6c1ed52249575
Filip Brzęk <filip.brzek@gmail.com>
2021-12-04 19:20:29 +0100

    revert: bug in zereoing socc-socc Hx block?

--- psi4/src/psi4/libscf_solver/rohf.cc
+++ psi4/src/psi4/libscf_solver/rohf.cc
@@ -758,12 +758,12 @@ void ROHF::Hx(SharedMatrix x, SharedMatrix ret) {
         double** rightp = Hx_right->pointer(h);
 
         for (size_t i = 0; i < soccpi_[h]; i++) {
-            // for (size_t j = 0; j < occpi[h]; j++) {
-            // leftp[j][i] = 0.0;
-            // }
-            for (size_t j = 0; j < soccpi_[h]; j++) {
-                leftp[doccpi_[h] + j][i] = 0.0;
+            for (size_t j = 0; j < occpi[h]; j++) {
+                leftp[j][i] = 0.0;
             }
+            // for (size_t j = 0; j < soccpi_[h]; j++) {
+            //    leftp[doccpi_[h] + j][i] = 0.0;
+            //}
             for (size_t j = 0; j < virpi[h]; j++) {
                 rightp[doccpi_[h] + i][j] = 0.0;
             }
